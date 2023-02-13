6fcbcea9141c5b8361867cf1481b2268bad5668a
Alastair Lloyd <dex8056@aol.com>
2023-02-13 13:43:44 -0700

    user story1

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with: "test123"
+    fill_in :user_password_confirmation, with: "test123"
     click_button 'Create New User'
 
     expect(current_path).to eq(user_path(User.last.id))
@@ -13,7 +15,7 @@ RSpec.describe "User Registration" do
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'notunique@example.com', password: "test123")
 
     visit register_path
     
